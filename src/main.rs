217f03d5a4ff26a684ae6f7b4b43f6335830e232
Oliver With <oliverwith@bbv.ch>
2024-05-23 00:01:03 +0200

    Check if model is ok, control visibility of "compete" depending on model

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
 }
 
+type DecisionModel = Vec<Parameter>;
+
 fn main() {
     use slint::Model;
 
@@ -26,16 +28,15 @@ fn main() {
     });
     main_window.set_results(results.into());
 
-    let mut parameter = Vec::<Parameter>::new();
+    let mut parameter = DecisionModel::new();
 
     let mww: slint::Weak<MainWindow> = main_window.as_weak();
     let im = input_model.clone();
     main_window.on_show(move || {
         sort_dedupe_clean_input(im.clone());
 
-        let mut s = SharedString::new();
         let vec_as_string = im.as_ref().iter().collect::<Vec<String>>().join("\n");
-
+        let mut s = SharedString::new();
         s.push_str(&vec_as_string);
         mww.unwrap().set_parameters(s);
     });
@@ -54,6 +55,16 @@ fn main() {
         println!("add params clicked:\n{}", parameters);
 
         im.extend(parameters.as_str().split('\n').map(str::to_string));
+    });
+
+    let im = input_model.clone();
+    main_window.on_play(move || {
+        sort_dedupe_clean_input(im.clone());
+
+        parameter.clear();
+        parameter.extend(prepare_model(im.clone()));
+
+        model_ok(&parameter)
     });
 
     let mmw = main_window.as_weak();
@@ -77,11 +88,26 @@ fn index_pairs(matrix_size: usize) -> Vec<(usize, usize)> {
 
 fn sort_dedupe_clean_input(input_model: Rc<VecModel<String>>) {
     let mut s: Vec<String> = input_model.as_ref().iter().collect();
-    s.iter_mut().for_each(|s| *s=s.trim().to_string());
+    s.iter_mut().for_each(|s| *s = s.trim().to_string());
     s.sort();
     s.retain(|i| i.ne(""));
     s.dedup_by(|a, b| a.eq_ignore_ascii_case(b));
     input_model.set_vec(s);
+}
+
+fn prepare_model(input_model: Rc<VecModel<String>>) -> DecisionModel {
+    input_model
+        .as_ref()
+        .iter()
+        .map(|s| Parameter {
+            name: s.into(),
+            score: 0,
+        })
+        .collect()
+}
+
+fn model_ok(model: &DecisionModel) -> bool {
+    model.len() >= 2 && model.iter().all(|x| x.score == 0)
 }
 
 #[cfg(test)]
@@ -89,7 +115,48 @@ mod tests {
     use super::*; // import functions from outer scope
 
     #[test]
-    fn dedupe_input_model() {
+    fn check_model_ok() {
+        let bad_model = vec![
+            Parameter {
+                name: "a".into(),
+                score: 0,
+            },
+            Parameter {
+                name: "b".into(),
+                score: 2,
+            },
+        ];
+
+        assert_eq!(false, model_ok(&bad_model));
+
+        let ok_model = vec![
+            Parameter {
+                name: "a".into(),
+                score: 0,
+            },
+            Parameter {
+                name: "b".into(),
+                score: 0,
+            },
+        ];
+
+        assert_eq!(true, model_ok(&ok_model));
+    }
+
+    #[test]
+    fn fill_input_model() {
+        let inner_input_model = vec_of_strings!["b", "a", "x"];
+        let input_model: Rc<VecModel<String>> = Rc::new(VecModel::<String>::default());
+        input_model.set_vec(inner_input_model);
+
+        let model = prepare_model(input_model.clone());
+        assert_eq!(3, model.len());
+        assert_eq!(0, model[2].score);
+        assert_eq!("x", model[2].name.as_str());
+    }
+
+    #[test]
+    fn clean_input_model() {
         let inner_input_model = vec_of_strings!["b", "a", "", "a", "  ix"];
         let input_model: Rc<VecModel<String>> = Rc::new(VecModel::<String>::default());
         input_model.set_vec(inner_input_model);
