38209d64e225f7d790464ca4e6071d2f4489789e
Oliver With <oliverwith@bbv.ch>
2024-06-14 10:48:49 +0200

    Move data type from gui to business logic
    - testability (without mocking all the ui types)
    - cleaner callbacks
    - separation of ui from business logic

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,167 @@
 use std::{cell::RefCell, rc::Rc};
 
-use slint::{Model, SharedString, VecModel};
+use slint::{SharedString, VecModel};
 
 slint::include_modules!();
 
-macro_rules! vec_of_strings {
-    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+
+enum Side {
+    Lhs,
+    Rhs,
+    Other,
 }
 
-type DecisionModel = Vec<Parameter>;
+#[derive(Default, PartialEq, Debug, Clone)]
+struct Parameters {
+    name: String,
+    score: u32,
+}
+
+struct DecisionModel {
+    parameters: RefCell<Vec<Parameters>>,
+    indices: RefCell<Vec<(usize, usize)>>,
+    current_index: RefCell<Option<usize>>,
+}
+impl DecisionModel {
+    fn new() -> Self {
+        Self {
+            parameters: RefCell::new(Vec::new()),
+            indices: RefCell::new(Vec::new()),
+            current_index: RefCell::new(None),
+        }
+    }
+
+    fn reset(&self) {
+        let mut params = self.parameters.borrow_mut();
+        let mut idx = self.indices.borrow_mut();
+        let mut ci = self.current_index.borrow_mut();
+
+        *params = Vec::new();
+        *idx = Vec::new();
+        *ci = None;
+    }
+
+    fn move_to_next_pair(&self) {
+        let mut ci = self.current_index.borrow_mut();
+        let idx = self.indices.borrow();
+
+        if ci.is_some() {
+            match idx.get(ci.unwrap() + 1) {
+                Some(_) => *ci = Some(ci.unwrap() + 1),
+                None => *ci = None,
+            };
+        }
+    }
+
+    fn get_current_pair(&self) -> Option<(Parameters, Parameters)> {
+        let ci = self.current_index.borrow();
+        let params = self.parameters.borrow();
+        let idx = self.indices.borrow();
+        match *ci {
+            Some(p) => Some((params[idx[p].0].clone(), params[idx[p].1].clone())),
+            None => None,
+        }
+    }
+
+    fn record_score_of_current_pair(&self, winner: Side, points_to_add: u32) {
+        let ci = self.current_index.borrow();
+        let mut params = self.parameters.borrow_mut();
+        let idx = self.indices.borrow();
+
+        if ci.is_some() {
+            match winner {
+                Side::Lhs => {
+                    params[idx[ci.unwrap()].0].score += points_to_add;
+                }
+                Side::Rhs => {
+                    params[idx[ci.unwrap()].1].score += points_to_add;
+                }
+                _ => {}
+            };
+        }
+    }
+
+    fn prepare_model(&self, param_list: &Vec<String>, randomize: bool) {
+        let mut parameters = self.parameters.borrow_mut();
+        for param in param_list {
+            parameters.push(Parameters {
+                name: param.to_string(),
+                score: 0,
+            });
+        }
+
+        parameters.sort_by(|a, b| a.name.cmp(&b.name));
+        parameters.retain(|i| i.name.ne(""));
+        parameters.dedup_by(|a, b| a.name.eq_ignore_ascii_case(&b.name));
+
+        let mut indices = self.indices.borrow_mut();
+        *indices = index_pairs(parameters.len());
+        if randomize {
+            let mut rng = rand::thread_rng();
+            rand::seq::SliceRandom::shuffle(&mut indices[..], &mut rng);
+        }
+
+        match parameters.len() > 1 {
+            true => {
+                let mut ci = self.current_index.borrow_mut();
+                *ci = Some(0);
+            }
+            false => {
+                let mut ci = self.current_index.borrow_mut();
+                *ci = None;
+            }
+        }
+    }
+
+    fn reset_score_and_indices(&self) {
+        let mut parameters: std::cell::RefMut<Vec<Parameters>> =
+            self.parameters.borrow_mut();
+        parameters.iter_mut().for_each(|p| p.score = 0);
+
+        if parameters.len() > 1 {
+            let mut ci = self.current_index.borrow_mut();
+            *ci = Some(0);
+        }
+    }
+
+    fn is_model_ready_to_play(&self) -> bool {
+        (self.parameters.borrow().len() >= 2)
+            && (self.parameters.borrow().iter().all(|x| x.score == 0))
+    }
+
+    fn get_parameters(&self) -> String {
+        self.parameters
+            .borrow()
+            .iter()
+            .map(|x| x.name.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+}
 
 fn main() {
-    use slint::Model;
-
     let main_window = MainWindow::new().unwrap();
-    let input_model = Rc::new(VecModel::<String>::default());
+    let model = Rc::new(DecisionModel::new());
 
-    let parameter = Rc::new(RefCell::new(DecisionModel::new()));
-    let combination_indices: Rc<RefCell<Vec<(usize, usize)>>> =
-        Rc::new(RefCell::new(Vec::<(usize, usize)>::new()));
-    let index_iter = Rc::new(RefCell::new(Vec::<(usize, usize)>::new().into_iter()));
-    let current_pair = Rc::new(RefCell::new(Option::<(usize, usize)>::default()));
-
-    let pm = parameter.clone();
-    let ci = combination_indices.clone();
-    let ii = index_iter.clone();
-    let cp = current_pair.clone();
     let mw = main_window.as_weak();
-    let im = input_model.clone();
-    main_window.on_dlg_play(move || {
-        pm.borrow_mut().clear();
-        pm.borrow_mut().extend(prepare_model(im.clone()));
+    let mdl = model.clone();
+    main_window.on_dialog_play(move || {
+        mdl.reset_score_and_indices();
 
-        ci.borrow_mut().clear();
+        let current_pair = mdl.get_current_pair().unwrap();
 
-        let mut rng = rand::thread_rng();
-        let mut indices = index_pairs(pm.borrow().len());
-        rand::seq::SliceRandom::shuffle(&mut indices[..], &mut rng);
-
-        ci.borrow_mut().extend(indices);
-        //update iterator
-        *ii.borrow_mut() = ci.borrow().clone().into_iter();
-
-        //todo: only yield when there is acutally playable parameters
-        let current_pair = ii.borrow_mut().next().unwrap();
-        *cp.borrow_mut() = Some(current_pair);
-
-        mw.unwrap()
-            .set_lhs_param(pm.borrow()[current_pair.0].clone());
-        mw.unwrap()
-            .set_rhs_param(pm.borrow()[current_pair.1].clone());
+        mw.unwrap().set_lhs_param_name(current_pair.0.name.into());
+        mw.unwrap().set_rhs_param_name(current_pair.1.name.into());
 
         mw.unwrap().set_edit_visible(false);
         mw.unwrap().set_compete_visible(true);
         mw.unwrap().set_result_visible(false);
-
-        println!("indices ({},{})", current_pair.0, current_pair.1);
     });
 
     let mw = main_window.as_weak();
-    let pm = parameter.clone();
-    let im = input_model.clone();
-    main_window.on_dlg_return_edit(move || {
-        pm.borrow_mut().clear();
-        pm.borrow_mut().extend(prepare_model(im.clone()));
+    let mdl = model.clone();
+    main_window.on_dialog_return_edit(move || {
+        mdl.reset_score_and_indices();
 
         mw.unwrap().set_edit_visible(true);
         mw.unwrap().set_compete_visible(false);
@@ -71,12 +169,18 @@ fn main() {
     });
 
     let mw = main_window.as_weak();
-    let pm = parameter.clone();
-    main_window.on_dlg_results(move || {
-        pm.borrow_mut().sort_by(|a, b| b.score.cmp(&a.score));
+    let mdl = model.clone();
+    main_window.on_dialog_results(move || {
+        let mut params: Vec<Parameters> = mdl.parameters.borrow().clone();
+        params.sort_by(|a, b| b.score.cmp(&a.score));
 
         let vm = Rc::new(VecModel::<Parameter>::default());
-        vm.set_vec(pm.borrow().clone());
+
+        vm.extend(params.into_iter().map(|i| Parameter {
+            name: i.name.into(),
+            score: i.score as i32,
+        }));
+
         mw.unwrap().set_results(vm.into());
 
         mw.unwrap().set_edit_visible(false);
@@ -84,93 +188,58 @@ fn main() {
         mw.unwrap().set_result_visible(true);
     });
 
-    let pm = parameter.clone();
-    let ii = index_iter.clone();
-    let cp = current_pair.clone();
     let mw = main_window.as_weak();
+    let mdl = model.clone();
     main_window.on_next_pair(move |winner| {
-        let next_indices = ii.borrow_mut().next();
-        let current_indices = *cp.borrow_mut();
-
-        match current_indices {
-            Some(c) => {
-                match winner {
-                    Winner::Lhs => {
-                        pm.borrow_mut()[c.0].score += 1;
-                        println!("lhs wins");
-                    }
-                    Winner::Rhs => {
-                        pm.borrow_mut()[c.1].score += 1;
-                        println!("rhs wins");
-                    }
-                    _ => {}
-                };
-            }
-            _ => {}
+        let winner_side: Side = match winner {
+            Winner::Lhs => Side::Lhs,
+            Winner::Rhs => Side::Rhs,
+            _ => Side::Other,
         };
 
-        match next_indices {
-            Some(n) => {
-                println!("indices ({}, {})", n.0, n.1);
+        mdl.record_score_of_current_pair(winner_side, 1);
 
-                mw.unwrap().set_lhs_param(pm.borrow()[n.0].clone());
-                mw.unwrap().set_rhs_param(pm.borrow()[n.1].clone());
+        mdl.move_to_next_pair();
 
-                *cp.borrow_mut() = Some(n);
+        match mdl.get_current_pair() {
+            Some(p) => {
+                mw.unwrap().set_lhs_param_name(p.0.name.into());
+                mw.unwrap().set_rhs_param_name(p.1.name.into());
             }
             None => {
-                mw.unwrap().set_lhs_param(Parameter {
-                    name: "--".into(),
-                    score: 0,
-                });
-                mw.unwrap().set_rhs_param(Parameter {
-                    name: "--".into(),
-                    score: 0,
-                });
-
+                mw.unwrap().set_lhs_param_name("--".into());
+                mw.unwrap().set_rhs_param_name("--".into());
                 mw.unwrap().set_results_enabled(true);
-
-                *cp.borrow_mut() = None;
             }
         }
     });
 
     let mw = main_window.as_weak();
-    let im = input_model.clone();
+    let mdl = model.clone();
     main_window.on_show(move || {
-        sort_dedupe_clean_input(im.clone());
-
-        let vec_as_string = im.as_ref().iter().collect::<Vec<String>>().join("\n");
-        let mut s = SharedString::new();
-        s.push_str(&vec_as_string);
-        mw.unwrap().set_parameters(s);
+        mw.unwrap()
+            .set_parameters(mdl.get_parameters().into());
     });
 
     let mw = main_window.as_weak();
-    let im = input_model.clone();
+    let mdl = model.clone();
     main_window.on_discard(move || {
-        im.set_vec(Vec::<String>::new());
         mw.unwrap().set_parameters(SharedString::new());
         mw.unwrap().set_play_enabled(false);
+
+        mdl.reset();
     });
 
     let mw = main_window.as_weak();
-    let pm = parameter.clone();
-    let im = input_model.clone();
+    let mdl = model.clone();
     main_window.on_append(move || {
-        let parameters = mw.unwrap().get_parameters();
-        println!("add params clicked:\n{}", parameters);
+        let parameters_ui = mw.unwrap().get_parameters();
+        println!("add params clicked:\n{}", parameters_ui);
 
-        im.extend(parameters.as_str().split('\n').map(str::to_string));
+        let parameter_list = clean_input(&parameters_ui);
+        mdl.prepare_model(&parameter_list, true);
 
-        sort_dedupe_clean_input(im.clone());
-
-        pm.borrow_mut().clear();
-        pm.borrow_mut().extend(prepare_model(im.clone()));
-
-        let model_ok = model_ok(&pm.borrow());
-
-        if model_ok {
+        if mdl.is_model_ready_to_play() {
             mw.unwrap().set_play_enabled(true);
         }
     });
@@ -179,21 +248,6 @@ fn main() {
     main_window.on_parameters_edited(move |new_text| mw.unwrap().set_parameters(new_text));
 
     main_window.run().unwrap();
-
-    let check_output: Vec<String> = input_model.as_ref().iter().collect();
-    let mdl = parameter
-        .borrow()
-        .iter()
-        .map(|n: &Parameter| n.name.to_string())
-        .collect::<Vec<String>>();
-    let scrs = parameter
-        .borrow()
-        .iter()
-        .map(|s: &Parameter| s.score)
-        .collect::<Vec<i32>>();
-
-    println!("at the end of the program:\n{:?}", check_output);
-    println!("model and rating\n{:?} - {:?}", mdl, scrs);
 }
 
 fn index_pairs(matrix_size: usize) -> Vec<(usize, usize)> {
@@ -206,28 +260,11 @@ fn index_pairs(matrix_size: usize) -> Vec<(usize, usize)> {
     indices
 }
 
-fn sort_dedupe_clean_input(input_model: Rc<VecModel<String>>) {
-    let mut s: Vec<String> = input_model.as_ref().iter().collect();
-    s.iter_mut().for_each(|s| *s = s.trim().to_string());
-    s.sort();
-    s.retain(|i| i.ne(""));
-    s.dedup_by(|a, b| a.eq_ignore_ascii_case(b));
-    input_model.set_vec(s);
-}
-
-fn prepare_model(input_model: Rc<VecModel<String>>) -> DecisionModel {
-    input_model
-        .as_ref()
-        .iter()
-        .map(|s| Parameter {
-            name: s.into(),
-            score: 0,
-        })
-        .collect()
-}
-
-fn model_ok(model: &DecisionModel) -> bool {
-    model.len() >= 2 && model.iter().all(|x| x.score == 0)
+fn clean_input(input: &str) -> Vec<String> {
+    let mut v: Vec<String> = input.split('\n').map(str::to_string).collect();
+    v = v.into_iter().map(|s| s.trim().to_string()).collect();
+    v.retain(|i| i.ne(""));
+    v
 }
 
 #[cfg(test)]
@@ -235,115 +272,94 @@ mod tests {
     use super::*; // import functions from outer scope
 
     #[test]
-    fn index_update() {
-        let combo_indices_basis: Vec<(usize, usize)> = Vec::new();
-        let combination_indices: Rc<RefCell<Vec<(usize, usize)>>> =
-            Rc::new(RefCell::new(combo_indices_basis));
-        let index_iter: Rc<RefCell<std::vec::IntoIter<(usize, usize)>>> = Rc::new(RefCell::new(
-            combination_indices.borrow().clone().into_iter(),
-        ));
-        let current_pair: Rc<RefCell<(usize, usize)>> = Rc::new(RefCell::new((0, 0)));
+    fn model_go_to_next_pair() {
+        let input: Vec<String> = vec!["a", "b", "c"].iter().map(|&s| s.to_string()).collect();
+        let model = DecisionModel::new();
+        model.prepare_model(&input, false);
+        model.move_to_next_pair();
 
-        let ci = combination_indices.clone();
-        let ii = index_iter.clone();
-        let cp = current_pair.clone();
-
-        let play = move || {
-            ci.borrow_mut().clear();
-            ci.borrow_mut().extend(index_pairs(3));
-
-            //update iterator
-            *ii.borrow_mut() = ci.borrow().clone().into_iter();
-            let current_pair = ii.borrow_mut().next();
-            *cp.borrow_mut() = current_pair.unwrap();
-        };
-
-        let ii = index_iter.clone();
-        let cp = current_pair.clone();
-        let yielding = move || {
-            let next_indices = ii.borrow_mut().next();
-            let next_indices = match next_indices {
-                Some(ix) => ix,
-                None => (0, 0),
-            };
-            *cp.borrow_mut() = next_indices;
-        };
-
-        play();
-        yielding();
-        yielding();
+        let current_model = model.get_current_pair().unwrap();
+        assert_eq!("c", current_model.1.name);
     }
 
     #[test]
-    fn check_model_ok() {
-        let bad_model = vec![
-            Parameter {
-                name: "a".into(),
-                score: 0,
-            },
-            Parameter {
-                name: "b".into(),
-                score: 2,
-            },
-        ];
+    fn model_increment_score() {
+        let input: Vec<String> = vec!["a", "b", "c"].iter().map(|&s| s.to_string()).collect();
+        let model = DecisionModel::new();
+        model.prepare_model(&input, false);
 
-        assert_eq!(false, model_ok(&bad_model));
+        model.record_score_of_current_pair(Side::Rhs, 15);
 
-        let ok_model = vec![
-            Parameter {
-                name: "a".into(),
-                score: 0,
-            },
-            Parameter {
-                name: "b".into(),
-                score: 0,
-            },
-        ];
-
-        assert_eq!(true, model_ok(&ok_model));
+        let current_model = model.get_current_pair().unwrap();
+        assert_eq!(15, current_model.1.score);
     }
 
     #[test]
-    fn fill_input_model() {
-        let inner_input_model = vec_of_strings!["b", "a", "x"];
-        let input_model: Rc<VecModel<String>> = Rc::new(VecModel::<String>::default());
-        input_model.set_vec(inner_input_model);
+    fn model_current_pair_good_model() {
+        let input: Vec<String> = vec!["a", "b", "c"].iter().map(|&s| s.to_string()).collect();
 
-        let model = prepare_model(input_model.clone());
-        assert_eq!(3, model.len());
-        assert_eq!(0, model[2].score);
-        assert_eq!("x", model[2].name.as_str());
-    }
+        let model = DecisionModel::new();
+        model.prepare_model(&input, false);
 
-    #[test]
-    fn clean_input_model() {
-        let inner_input_model = vec_of_strings!["b", "a", "", "a", "  ix"];
-        let input_model: Rc<VecModel<String>> = Rc::new(VecModel::<String>::default());
-        input_model.set_vec(inner_input_model);
-
-        sort_dedupe_clean_input(input_model.clone());
-
-        let inner_test_model: Vec<String> = input_model.as_ref().iter().collect();
-        assert_eq!("a", inner_test_model[0]);
-        assert_eq!("ix", inner_test_model[2]);
-        assert_eq!(3, inner_test_model.len());
-    }
-    #[test]
-    fn index_generation() {
-        assert_eq!(0, index_pairs(0).len());
-        assert_eq!(0, index_pairs(1).len());
-        assert_eq!(1, index_pairs(2).len());
-        assert_eq!((0, 1), index_pairs(2)[0]);
-
-        assert_eq!(3, index_pairs(3).len());
-        assert_eq!((0, 1), index_pairs(3)[0]);
-        assert_eq!((0, 2), index_pairs(3)[1]);
-        assert_eq!((1, 2), index_pairs(3)[2]);
-
-        const HI_INDEX: usize = 8;
         assert_eq!(
-            (HI_INDEX - 2, HI_INDEX - 1),
-            index_pairs(HI_INDEX)[((HI_INDEX * (HI_INDEX - 1)) / 2) - 1]
+            (
+                Parameters {
+                    name: "a".to_string(),
+                    score: 0
+                },
+                Parameters {
+                    name: "b".to_string(),
+                    score: 0
+                }
+            ),
+            model.get_current_pair().unwrap()
         );
+    }
+
+    #[test]
+    fn model_current_pair_bad_model() {
+        let input: Vec<String> = vec!["a"].iter().map(|&s| s.to_string()).collect();
+
+        let model = DecisionModel::new();
+        model.prepare_model(&input, false);
+
+        assert_eq!(None, model.get_current_pair());
+    }
+
+    #[test]
+    fn model_parameter_list() {
+        let input: Vec<String> = vec!["a", "miao", "x"]
+            .iter()
+            .map(|&s| s.to_string())
+            .collect();
+
+        let model = DecisionModel::new();
+        model.prepare_model(&input, false);
+
+        assert_eq!("a\nmiao\nx", model.get_parameters());
+    }
+    #[test]
+    fn model_prepare() {
+        let input: Vec<String> = vec!["a", "miao", "x"]
+            .iter()
+            .map(|&s| s.to_string())
+            .collect();
+
+        let model = DecisionModel::new();
+        model.prepare_model(&input, false);
+
+        assert_eq!(true, model.is_model_ready_to_play());
+    }
+
+    #[test]
+    fn model_dedupe() {
+        let expected: Vec<String> = vec!["a", "miao", "x"]
+            .iter()
+            .map(|&s| s.to_string())
+            .collect();
+
+        let input = String::from("a \nmiao\n\nx\n");
+        let result = clean_input(&input);
+        assert_eq!(expected, result);
     }
 }
